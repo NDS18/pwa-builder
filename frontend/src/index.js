54e5ed414725e723b36097a703eafe80e2c25fde
Максим Чумак <nds@MacBook-Pro-Maksim.local>
2025-07-06 18:39:24 +0700

    Fix: Remove unused useCallback import

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App, { AuthProvider } from './App';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <App />
   </React.StrictMode>
 );
